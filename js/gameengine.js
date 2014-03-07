62be3cb6351ba0846b0357a072880c9bed2861a0
nbleisch <nbleisch@lichtflut.de>
2014-03-07 19:34:16 +0100

    Adding first type of collision detection

--- js/gameengine.js
+++ js/gameengine.js
@@ -6,21 +6,45 @@
 		}
 
 		this.doNextStep = function(){
-	      if(player.moveDirection!=null){
-	      	if(player.moveDirection == "forward"){
-	        	player.x += Math.cos(Math.PI/180 * player.orientation)*player.velocity;
-	        	player.y += Math.sin(Math.PI/180 * player.orientation)*player.velocity;
-	    	}else if(player.moveDirection == "backward"){
-	        	player.x -= Math.cos(Math.PI/180 * player.orientation)*player.velocity;
-	        	player.y -= Math.sin(Math.PI/180 * player.orientation)*player.velocity;
-	    	}else if(player.moveDirection == "left"){
-	        	player.x += Math.cos(Math.PI/180 * (player.orientation - 90))*player.velocity;
-	        	player.y += Math.sin(Math.PI/180 * (player.orientation - 90))*player.velocity;
-	    	}else if(player.moveDirection == "right"){
-	        	player.x += Math.cos(Math.PI/180 * (player.orientation + 90))*player.velocity;
-	        	player.y += Math.sin(Math.PI/180 * (player.orientation + 90))*player.velocity;
-	    	}
-	       }
+		  for (var i = 0; i < mapObjects.length; i++){         
+		      if(mapObjects[i].moveDirection!=null){
+		      	var movementDeltaX=0;
+			  	var movementDeltaY=0;
+
+		      	if(mapObjects[i].moveDirection == "forward"){
+		        	movementDeltaX += Math.cos(Math.PI/180 * mapObjects[i].orientation)*mapObjects[i].velocity;
+		        	movementDeltaY += Math.sin(Math.PI/180 * mapObjects[i].orientation)*mapObjects[i].velocity;
+		    	}else if(mapObjects[i].moveDirection == "backward"){
+		        	movementDeltaX -= Math.cos(Math.PI/180 * mapObjects[i].orientation)*mapObjects[i].velocity;
+		        	movementDeltaY -= Math.sin(Math.PI/180 * mapObjects[i].orientation)*mapObjects[i].velocity;
+		    	}else if(mapObjects[i].moveDirection == "left"){
+		        	movementDeltaX += Math.cos(Math.PI/180 * (mapObjects[i].orientation - 90))*mapObjects[i].velocity;
+		        	movementDeltaY += Math.sin(Math.PI/180 * (mapObjects[i].orientation - 90))*mapObjects[i].velocity;
+		    	}else if(mapObjects[i].moveDirection == "right"){
+		        	movementDeltaX += Math.cos(Math.PI/180 * (mapObjects[i].orientation + 90))*mapObjects[i].velocity;
+		        	movementDeltaY += Math.sin(Math.PI/180 * (mapObjects[i].orientation + 90))*mapObjects[i].velocity;
+		    	}
+		    	var nextPositionX = mapObjects[i].x + movementDeltaX;
+		    	var nextPositionY = mapObjects[i].y + movementDeltaY;
+		    	//lets do some collision detection
+		    	var isCollided = false;
+		    	for (var j = 0; j < mapObjects.length; j++){
+		    		if(i!=j){
+		    			var dx = nextPositionX - mapObjects[j].x;
+		    			var dy = nextPositionY - mapObjects[j].y;
+		    			var combinedRadius = (mapObjects[i].radius + mapObjects[j].radius);
+		    			//optimized pythagoras
+		    			if(((dx * dx) + (dy * dy)) < (combinedRadius * combinedRadius)){
+		    				isCollided=true;
+		    			}
+		    		}
+		    	}
+		    	if(!isCollided){
+		    		mapObjects[i].x = nextPositionX;
+		    		mapObjects[i].y = nextPositionY;
+		    	}
+		       }
+	        }
 	    }
 	}
 
